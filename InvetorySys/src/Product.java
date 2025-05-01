e158c43ba7a5167b4bb776319678460b67945451
Chayandeep <chayansingh05@gmail.com>
2025-05-01 18:19:03 -0400

    making a template for code

--- InvetorySys/src/Product.java
+++ InvetorySys/src/Product.java
@@ -1,4 +1,40 @@
 
 public class Product {
-
+	private int id;
+	private String description;
+	private String name;
+	private String category;
+	private String brand;
+	
+	public void getId(){
+		this.id = id;
+	}
+	
+	public void getDecsription() {
+		this.description = description;
+	}
+	
+	public void getName() {
+		this.name = name;
+	}
+	
+	public void getCategory() {
+		this.category = category;
+	}
+	
+	public void getBrand() {
+		this.brand = brand;
+	}
+	
+	public void addProduct(String name, String description, String category, String brand){
+	
+	}
+	
+	public void editProduct(String name, String description, String category, String brand) {
+		
+	}
+	
+	public void deleteProduct(int id) {
+		
+	}
 }
---
+++ InvetorySys/src/Return.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Return extends Product{
+	private int quant;
+	private boolean availability;
+	private double cost;
+	private Date datein;
+
+}
