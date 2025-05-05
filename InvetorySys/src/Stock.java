61695d9bb180c09dc2acaec602b820187ab2014f
Chayandeep <chayansingh05@gmail.com>
2025-05-04 23:13:57 -0400

    Worked on InStock class

--- InvetorySys/src/Stock.java
+++ InvetorySys/src/Stock.java
@@ -13,4 +13,44 @@ public class Stock extends Product{
 		
 	}
 	
+	public boolean getAvailability() {
+		return availability;
+	}
+	
+	public double getCost() {
+		return cost;
+	}
+	
+	public int getQuant() {
+		return quant;
+	}
+	
+	public void addStock(double cost, int quant) {
+		this.cost = cost;
+		this.quant += quant;
+		this.availability = true;
+	}
+	
+	public void editStock(int id, double cost, int quant) {
+		if(this.getId()== id) {
+			this.cost = cost;
+			this.quant = quant;
+		}
+	}
+	
+	public void deleteStock(int id) {
+		if(this.getId()== id) {
+			this.quant = 0;
+			this.availability = false;
+		}
+	}
+	
+	@Override
+    public String toString() {
+        return "Product ID: " + getId() +
+               ", Quantity: " + quant +
+               ", Cost: " + cost +
+               ", Availability: " + availability;
+    }
+	
 }
