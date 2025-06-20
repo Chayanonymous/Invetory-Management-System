886934a35963bcfa25c16a0e747bf711f1adfdea
Chayandeep <chayansingh05@gmail.com>
2025-06-19 20:17:28 -0400

    added additional files

---
+++ InvetorySys/src/Category.java
@@ -0,0 +1,19 @@
+
+public class Category {
+	
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void addCategory(String name) {
+		this.name = name;
+	}
+
+}
